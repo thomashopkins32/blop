1c7b908b3e7288730c3e0c0acfaa3c332f6223e1
thomashopkins32 <thopkins1@bnl.gov>
2025-01-13 13:58:15 -0500

    Added ophyd-async signals for simulated beamline

--- src/blop/sim/async_beamline.py
+++ src/blop/sim/async_beamline.py
@@ -1,136 +1,81 @@
-import itertools
-from collections import deque
-from datetime import datetime
-from pathlib import Path
-from typing import Annotated
-
-import h5py # type: ignore
 import numpy as np
 import scipy as sp # type: ignore
-from event_model import compose_resource # type: ignore
-from ophyd_async.core import Device, SignalR, SignalRW, StandardReadableFormat
-
+from ophyd_async.core import StandardReadable, soft_signal_r_and_setter, StandardReadableFormat
+from bluesky.protocols import Triggerable
 from ..utils import get_beam_stats
-from .handlers import ExternalFileReference
 
 
-class Detector(Device):
-    sum = Annotated[SignalRW, StandardReadableFormat.HINTED]
-    max = Annotated[SignalRW, StandardReadableFormat.NORMAL]
-    area = Annotated[SignalRW, StandardReadableFormat.NORMAL]
-    cen_x = Annotated[SignalRW, StandardReadableFormat.HINTED]
-    cen_y = Annotated[SignalRW, StandardReadableFormat.HINTED]
-    wid_x = Annotated[SignalRW, StandardReadableFormat.HINTED]
-    wid_y = Annotated[SignalRW, StandardReadableFormat.HINTED]
-    image = Annotated[ExternalFileReference, StandardReadableFormat.NORMAL]
-    image_shape = Cpt(Signal, value=(300, 400), kind="normal")
-    noise = Cpt(Signal, kind="normal")
-
-    def __init__(self, root_dir: str = "/tmp/blop/sim", verbose: bool = True, noise: bool = True, *args, **kwargs):
+class SimDetector(StandardReadable, Triggerable):
+    def __init__(self, noise: bool = True, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        with self.add_children_as_readables():
+            self.max, self.set_max = soft_signal_r_and_setter(float)
+            self.area, self.set_area = soft_signal_r_and_setter(float)
+            self.image, self.set_image = soft_signal_r_and_setter(np.ndarray)
+            self.image_shape, self.set_image_shape = soft_signal_r_and_setter(np.ndarray, initial_value=np.array([300, 400]))
+            self.noise, self.set_noise = soft_signal_r_and_setter(bool, initial_value=noise)
 
-        _ = make_dir_tree(datetime.now().year, base_path=root_dir)
+        with self.add_children_as_readables(format=StandardReadableFormat.HINTED_SIGNAL):
+            self.sum, self.set_sum = soft_signal_r_and_setter(float)
+            self.cen_x, self.set_cen_x = soft_signal_r_and_setter(int)
+            self.cen_y, self.set_cen_y = soft_signal_r_and_setter(int)
+            self.wid_x, self.set_wid_x = soft_signal_r_and_setter(int)
+            self.wid_y, self.set_wid_y = soft_signal_r_and_setter(int)
 
-        self._root_dir = root_dir
-        self._verbose = verbose
-
-        # Used for the emulated cameras only.
-        self._img_dir = None
-
-        # Resource/datum docs related variables.
-        self._asset_docs_cache = deque()
-        self._resource_document = None
-        self._datum_factory = None
-
-        self.noise.put(noise)
-
-    def trigger(self):
-        super().trigger()
-        raw_image = self.generate_beam(noise=self.noise.get())
+    async def trigger(self):
+        raw_image = await self.generate_beam(noise=await self.noise.get_value())
 
         current_frame = next(self._counter)
 
-        self._dataset.resize((current_frame + 1, *self.image_shape.get()))
+        image_shape = await self.image_shape.get_value()
 
-        self._dataset[current_frame, :, :] = raw_image
+        self._dataset.resize((current_frame + 1, *image_shape))
+        self._dataset.get()[current_frame, :, :] = raw_image
 
-        datum_document = self._datum_factory(datum_kwargs={"frame": current_frame})
-        self._asset_docs_cache.append(("datum", datum_document))
 
         stats = get_beam_stats(raw_image)
-        self.image.put(datum_document["datum_id"])
 
-        for attr in ["max", "sum", "cen_x", "cen_y", "wid_x", "wid_y"]:
-            getattr(self, attr).put(stats[attr])
+        await self.set_max(stats["max"])
+        await self.set_sum(stats["sum"])
+        await self.set_cen_x(stats["cen_x"])
+        await self.set_cen_y(stats["cen_y"])
+        await self.set_wid_x(stats["wid_x"])
+        await self.set_wid_y(stats["wid_y"])
 
-        super().trigger()
+        await self.set_image(raw_image)
 
-        return NullStatus()
+        return None
 
-    def stage(self):
-        super().stage()
-        date = datetime.now()
-        self._assets_dir = date.strftime("%Y/%m/%d")
-        data_file = f"{new_uid()}.h5"
-
-        self._resource_document, self._datum_factory, _ = compose_resource(
-            start={"uid": "needed for compose_resource() but will be discarded"},
-            spec="HDF5",
-            root=self._root_dir,
-            resource_path=str(Path(self._assets_dir) / Path(data_file)),
-            resource_kwargs={},
-        )
-
-        self._data_file = str(Path(self._resource_document["root"]) / Path(self._resource_document["resource_path"]))
-
-        # now discard the start uid, a real one will be added later
-        self._resource_document.pop("run_start")
-        self._asset_docs_cache.append(("resource", self._resource_document))
-
-        self._h5file_desc = h5py.File(self._data_file, "x")
-        group = self._h5file_desc.create_group("/entry")
-        self._dataset = group.create_dataset(
-            "image",
-            data=np.full(fill_value=np.nan, shape=(1, *self.image_shape.get())),
-            maxshape=(None, *self.image_shape.get()),
-            chunks=(1, *self.image_shape.get()),
-            dtype="float64",
-            compression="lzf",
-        )
-        self._counter = itertools.count()
-
-    def unstage(self):
-        super().unstage()
-        del self._dataset
-        self._h5file_desc.close()
-        self._resource_document = None
-        self._datum_factory = None
-
-    def collect_asset_docs(self):
-        items = list(self._asset_docs_cache)
-        self._asset_docs_cache.clear()
-        yield from items
-
-    def generate_beam(self, noise: bool = True):
-        nx, ny = self.image_shape.get()
+    async def generate_beam(self, noise: bool = True):
+        nx, ny = self.image_shape.get_value()
 
         x = np.linspace(-10, 10, ny)
         y = np.linspace(-10, 10, nx)
         X, Y = np.meshgrid(x, y)
 
-        x0 = self.parent.kbh_ush.get() - self.parent.kbh_dsh.get()
-        y0 = self.parent.kbv_usv.get() - self.parent.kbv_dsv.get()
-        x_width = np.sqrt(0.2 + 5e-1 * (self.parent.kbh_ush.get() + self.parent.kbh_dsh.get() - 1) ** 2)
-        y_width = np.sqrt(0.1 + 5e-1 * (self.parent.kbv_usv.get() + self.parent.kbv_dsv.get() - 2) ** 2)
+        kbh_ush = await self.parent.kbh_ush.get_value()
+        kbh_dsh = await self.parent.kbh_dsh.get_value()
+        kbv_usv = await self.parent.kbv_usv.get_value()
+        kbv_dsv = await self.parent.kbv_dsv.get_value()
+
+        x0 = kbh_ush - kbh_dsh
+        y0 = kbv_usv - kbv_dsv
+        x_width = np.sqrt(0.2 + 5e-1 * (kbh_ush + kbh_dsh - 1) ** 2)
+        y_width = np.sqrt(0.1 + 5e-1 * (kbv_usv + kbv_dsv - 2) ** 2)
 
         beam = np.exp(-0.5 * (((X - x0) / x_width) ** 4 + ((Y - y0) / y_width) ** 4)) / (
             np.sqrt(2 * np.pi) * x_width * y_width
         )
 
-        mask = X > self.parent.ssa_inboard.get()
-        mask &= X < self.parent.ssa_outboard.get()
-        mask &= Y > self.parent.ssa_lower.get()
-        mask &= Y < self.parent.ssa_upper.get()
+        ssa_inboard = await self.parent.ssa_inboard.get_value()
+        ssa_outboard = await self.parent.ssa_outboard.get_value()
+        ssa_lower = await self.parent.ssa_lower.get_value()
+        ssa_upper = await self.parent.ssa_upper.get_value()
+
+        mask = X > ssa_inboard
+        mask &= X < ssa_outboard
+        mask &= Y > ssa_lower
+        mask &= Y < ssa_upper
         mask = sp.ndimage.gaussian_filter(mask.astype(float), sigma=1)
 
         image = beam * mask
@@ -151,18 +96,16 @@ class Detector(Device):
         return image
 
 
-class Beamline(Device):
-    det = Cpt(Detector)
-
-    kbh_ush = Cpt(Signal, kind="hinted")
-    kbh_dsh = Cpt(Signal, kind="hinted")
-    kbv_usv = Cpt(Signal, kind="hinted")
-    kbv_dsv = Cpt(Signal, kind="hinted")
-
-    ssa_inboard = Cpt(Signal, value=-5.0, kind="hinted")
-    ssa_outboard = Cpt(Signal, value=5.0, kind="hinted")
-    ssa_lower = Cpt(Signal, value=-5.0, kind="hinted")
-    ssa_upper = Cpt(Signal, value=5.0, kind="hinted")
-
-    def __init__(self, *args, **kwargs):
+class Beamline(StandardReadable):
+    def __init__(self, noise: bool = False, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.det = SimDetector(noise=noise)
+        with self.add_children_as_readables(format=StandardReadableFormat.HINTED_SIGNAL):
+            self.kbh_ush, self.set_kbh_ush = soft_signal_r_and_setter(float)
+            self.kbh_dsh, self.set_kbh_dsh = soft_signal_r_and_setter(float)
+            self.kbv_usv, self.set_kbv_usv = soft_signal_r_and_setter(float)
+            self.kbv_dsv, self.set_kbv_dsv = soft_signal_r_and_setter(float)
+            self.ssa_inboard, self.set_ssa_inboard = soft_signal_r_and_setter(float, initial_value=-5.0)
+            self.ssa_outboard, self.set_ssa_outboard = soft_signal_r_and_setter(float, initial_value=5.0)
+            self.ssa_lower, self.set_ssa_lower = soft_signal_r_and_setter(float, initial_value=-5.0)
+            self.ssa_upper, self.set_ssa_upper = soft_signal_r_and_setter(float, initial_value=5.0)
